10e5febca5bb6db40aefbfecc6c5f806a795ff44
István Gansperger <gansperger@gmail.com>
2016-02-02 16:43:56 +0100

    Build with zafir

--- build.fsx
+++ build.fsx
@@ -3,8 +3,8 @@ open IntelliFactory.Build
 open System.IO
 
 let bt =
-    BuildTool().PackageId("WebSharper.Templates")
-        .VersionFrom("WebSharper")
+    BuildTool().PackageId("Zafir.Templates")
+        .VersionFrom("Zafir")
         .WithFSharpVersion(FSharpVersion.FSharp31)
         .WithFramework(fun fw -> fw.Net40)
 
@@ -24,10 +24,10 @@ bt.Solution [
     bt.NuGet.CreatePackage()
         .Configure(fun c ->
             { c with
-                Title = Some "WebSharper.Templates"
+                Title = Some "Zafir.Templates"
                 LicenseUrl = Some "http://websharper.com/licensing"
                 ProjectUrl = Some "https://github.com/intellifactory/websharper.visualstudio"
-                Description = "WebSharper Project Templates"
+                Description = "Zafir Project Templates"
                 RequiresLicenseAcceptance = true })
         .Add(templates)
     |> Array.foldBack (fun f n -> n.AddFile(f)) (
