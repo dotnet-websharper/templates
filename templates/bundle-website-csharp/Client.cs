30e71c6f9675fd3da4fe7228a73103e932db4ba7
István Gansperger <gansperger@gmail.com>
2016-02-02 16:44:24 +0100

    Add C# SPA template files

---
+++ templates/bundle-website-csharp/Client.cs
@@ -0,0 +1,15 @@
+using WebSharper;
+using WebSharper.JavaScript;
+using static WebSharper.Core.Attributes;
+
+namespace $safeprojectname$
+{
+    public class Client
+    {
+        [SPAEntryPoint]
+        public static void Main()
+        {
+            Console.Log("Running JavaScript Entry Point..");
+        }
+    }
+}
