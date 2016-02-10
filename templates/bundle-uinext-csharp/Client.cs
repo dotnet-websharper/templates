b21059413d294f40bdbdab0c64b42debbd1cecd0
András Jankó <andras.janko@gmail.com>
2016-02-10 21:15:27 +0100

    a temporary UI.Next SPA template

--- templates/bundle-uinext-csharp/Client.cs
+++ templates/bundle-uinext-csharp/Client.cs
@@ -1,16 +1,41 @@
+using Microsoft.FSharp.Core;
+using System;
 using WebSharper;
-using WebSharper.JavaScript;
+using WebSharper.UI.Next;
 using static WebSharper.Core.Attributes;
+using static WebSharper.UI.Next.Client.DocExtensions;
+using Dom = WebSharper.JavaScript.Dom;
+using JSConsole = WebSharper.JavaScript.Console;
 
 namespace $safeprojectname$
 {
-    public class Client
+    [JavaScript]
+    public class App
     {
-        [JavaScript]
+        [Inline("function (x) { $wsruntime.InvokeDelegate($f, [x]) }")]
+        private static FSharpFunc<T, Unit> ConvertMethod<T>(Action<T> f) => null;
+
+        private static FSharpFunc<T1, FSharpFunc<T2, Unit>> ConvertMethod<T1, T2>(Action<T1, T2> f)
+            => ConvertMethod((T1 x) => ConvertMethod((T2 y) => f(x, y)));
+
+        [Inline("function (x) { return $wsruntime.InvokeDelegate($f, [x]) }")]
+        private static FSharpFunc<T, U> ConvertMethod<T, U>(Func<T, U> f) => null;
+
+        private static FSharpFunc<T1, FSharpFunc<T2, U>> ConvertMethod<T1, T2, U>(Func<T1, T2, U> f)
+            => ConvertMethod((T1 x) => ConvertMethod((T2 y) => f(x, y)));
+
         [SPAEntryPoint]
         public static void Main()
         {
-            Console.Log("Running JavaScript Entry Point..");
+            var name = Var.Create("...");
+
+            WebSharper.UI.Next.Client.Doc.RunById("main",
+                Html.div(new[] {
+                    Html.div( new[] { Html.text("Hello "), Html.textView(name.View) }),
+                    Html.button (new [] { Html.text("Click me!") } )
+                        .OnClick(ConvertMethod((Dom.Element el, Dom.MouseEvent ev) => { name.Value = "world!"; }))
+                })
+            );
         }
     }
 }
