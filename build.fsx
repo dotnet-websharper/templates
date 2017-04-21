23debc607454bf4cb6f2cf0c44bebfddac5f1d79
Jand42 <andras.janko@gmail.com>
2017-04-21 17:33:11 +0200

    fix version of SharpCompress used

--- build.fsx
+++ build.fsx
@@ -15,7 +15,7 @@ let templates =
             [
                 r.Assembly("System.Xml")
                 r.Assembly("System.Xml.Linq")
-                r.NuGet("SharpCompress").ForceFoundVersion().Reference()
+                r.NuGet("SharpCompress").Version("[0.11.6]").ForceFoundVersion().Reference()
             ])
 
 bt.Solution [
