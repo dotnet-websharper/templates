23a1ae61e69625fbdc1b19e4ea217503b04e93f2
Jand42 <andras.janko@gmail.com>
2017-05-12 17:42:45 +0200

    fixes to build script

--- init.fsx
+++ init.fsx
@@ -5,17 +5,26 @@ open System.Text.RegularExpressions
 
 let (+/) a b = Path.Combine(a, b)
 
-Directory.EnumerateFiles("packages", "*.nupkg", SearchOption.AllDirectories)
+let pkgFolder = __SOURCE_DIRECTORY__ +/ "WebSharper/Packages"
+if not <| Directory.Exists pkgFolder then
+    Directory.CreateDirectory pkgFolder |> ignore
+
+Directory.EnumerateFiles(__SOURCE_DIRECTORY__ +/ "packages", "*.nupkg", SearchOption.AllDirectories)
 |> Seq.iter (fun p ->
-    File.Copy(p, "WebSharper/Packages" +/ Path.GetFileName(p), true) 
+    File.Copy(p, pkgFolder +/ Path.GetFileName(p), true) 
 )
 
 let packageVersions =
-    let packageRegex = Regex "    ([^ ]+) \(([^\)]+)\)"
+    let packageRegex = Regex "([^ ]+) \(([^\)>=]+)\)"
     File.ReadAllLines("paket.lock") |> Seq.choose (fun l ->
         let c = packageRegex.Match l
         if c.Success then
-            Some (c.Groups.[1].Value, c.Groups.[2].Value)
+            let p = c.Groups.[1].Value
+            let v = c.Groups.[2].Value
+            if p = "System.ValueTuple" then
+                Some (p, "4.3.0")
+            else
+                Some (p, v)
         else
             None
     ) |> List.ofSeq
@@ -31,7 +40,7 @@ let replaces =
                 sprintf "Include=\"Packages\\%s.%s.nupkg\"" p v
     ]
 
-Directory.EnumerateFiles(".", "*.in", SearchOption.AllDirectories)
+Directory.EnumerateFiles(__SOURCE_DIRECTORY__, "*.in", SearchOption.AllDirectories)
 |> Seq.iter (fun p ->
     let inp = File.ReadAllText(p)
     let res =
