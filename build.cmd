53041a7ff15614ef68d78ac02f922b1a9e1bb386
András Jankó <andras.janko@gmail.com>
2021-01-08 22:33:34 +0100

    further cleanup and build fixes

--- build.cmd
+++ build.cmd
@@ -5,4 +5,4 @@ dotnet tool restore
 dotnet paket update
 if errorlevel 1 exit /b %errorlevel%
     
-call paket-files\wsbuild\github.com\dotnet-websharper\build-script\build.cmd %*
+dotnet fake run build.fsx -t %*
