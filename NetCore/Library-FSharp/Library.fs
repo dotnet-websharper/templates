9b6e4ef6974f626f4a14a8102e88e3bc97ff737b
András Jankó <andras.janko@gmail.com>
2021-01-11 15:55:50 +0100

    add csproj to package for dotnet new

--- NetCore/Library-FSharp/Library.fs
+++
@@ -1,7 +0,0 @@
-﻿namespace WebSharper.Library.FSharp
-
-open WebSharper
-
-[<JavaScript>]
-type Class1() = 
-    member this.X = "F#"
--- build.cmd
+++ build.cmd
@@ -3,6 +3,14 @@ setlocal
     
 dotnet tool restore
 dotnet paket update
-if errorlevel 1 exit /b %errorlevel%
-    
-dotnet fake run build.fsx %*
+if errorlevel 1 exit /b %errorlevel%    
+
+:: Allow running `build SomeTask` instead of `build -t SomeTask`
+set _Add-t=""
+set FirstArg=%1
+if not "%FirstArg%"=="" if not "%FirstArg:~0,1%"=="-" set _Add-t=1
+if "%_Add-t%"=="1" (
+  dotnet fake run build.fsx -t %*
+) else (
+  dotnet fake run build.fsx %*
+)
