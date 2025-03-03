b2f898d0d4af7554cf14b11d93cfb10b9e965d34
András Jankó <andras.janko@gmail.com>
2025-03-03 14:14:42 +0100

    #53 offline templates

---
+++ WebSharper.Templates/templates/Offline-CSharp/esbuild.config.mjs
@@ -0,0 +1,23 @@
+﻿import { readdirSync, cpSync } from 'fs'
+import { build } from 'esbuild'
+
+cpSync('./build/', './wwwroot/', { recursive: true, filter: source => source.endsWith('build') || source.endsWith('.html') });
+
+const files = readdirSync('./build/Scripts/WebSharper/WebSharper.Offline.CSharp/');
+
+files.forEach(file => {
+  if (file.endsWith('.js')) {
+    var options =
+    {
+      entryPoints: ['./build/Scripts/WebSharper/WebSharper.Offline.CSharp/' + file],
+      bundle: true,
+      minify: true,
+      format: 'iife',
+      outfile: 'wwwroot/Scripts/WebSharper/' + file,
+      globalName: 'wsbundle'
+    };
+
+    console.log("Bundling:", file);
+    build(options);
+  }
+});
