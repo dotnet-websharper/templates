4a7abdb5e45bd690cd900d341a8fa329ae16885f
András Jankó <andras.janko@gmail.com>
2025-03-03 14:56:12 +0100

    #53 offline fixes

--- WebSharper.Templates/templates/Offline-CSharp/esbuild.config.mjs
+++ WebSharper.Templates/templates/Offline-CSharp/esbuild.config.mjs
@@ -1,7 +1,7 @@
 ﻿import { readdirSync, cpSync } from 'fs'
 import { build } from 'esbuild'
 
-cpSync('./build/', './wwwroot/', { recursive: true, filter: source => source.endsWith('build') || source.endsWith('.html') });
+cpSync('./build/', './bin/html/', { recursive: true, filter: source => source.endsWith('build') || source.endsWith('.html') });
 
 const files = readdirSync('./build/Scripts/WebSharper/WebSharper.Offline.CSharp/');
 
@@ -13,7 +13,7 @@ files.forEach(file => {
       bundle: true,
       minify: true,
       format: 'iife',
-      outfile: 'wwwroot/Scripts/WebSharper/' + file,
+      outfile: 'bin/html/Scripts/WebSharper/' + file,
       globalName: 'wsbundle'
     };
 
