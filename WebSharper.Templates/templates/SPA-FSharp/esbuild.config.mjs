e949f70693836103040517ce63b1fe21942d18ef
András Jankó <andras.janko@gmail.com>
2025-03-26 10:19:42 +0100

    #53 fixes for spa esbuild

--- WebSharper.Templates/templates/SPA-FSharp/esbuild.config.mjs
+++ WebSharper.Templates/templates/SPA-FSharp/esbuild.config.mjs
@@ -1,21 +1,17 @@
 ﻿import { existsSync, cpSync, readdirSync } from 'fs'
 import { build } from 'esbuild'
 
-if (existsSync('./build/Content/WebSharper/')) {
-  cpSync('./build/Content/WebSharper/', './wwwroot/Content/WebSharper/', { recursive: true });
-}
-
-const files = readdirSync('./build/Scripts/WebSharper/WebSharper.SPA.FSharp/');
+const files = readdirSync('./build/');
 
 files.forEach(file => {
   if (file.endsWith('.js')) {
     var options =
     {
-      entryPoints: ['./build/Scripts/WebSharper/WebSharper.SPA.FSharp/' + file],
+      entryPoints: ['./build/' + file],
       bundle: true,
       minify: true,
       format: 'iife',
-      outfile: 'wwwroot/Scripts/WebSharper/' + file,
+      outfile: 'wwwroot/Scripts/' + file,
       globalName: 'wsbundle'
     };
 
